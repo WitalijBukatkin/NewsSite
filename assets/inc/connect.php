f11f028137830ea58f8a6b169249569585b8ab67
Witalij Bukatkin <32595013+WitalijBukatkin@users.noreply.github.com>
2018-12-06 18:51:29 +0300

    Update connect.php

--- assets/inc/connect.php
+++ assets/inc/connect.php
@@ -1,4 +1,4 @@
 <?php
-$db=new mysqli("localhost:3306", "root");
-$db->select_db('newssite');
+$db=new mysqli("eu-cdbr-west-02.cleardb.net", "b805059e2ed673", "ea3fe87b");
+$db->select_db('heroku_38f92d8abb6528c');
 $db->set_charset('utf8');
